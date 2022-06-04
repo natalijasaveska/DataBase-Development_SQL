d963122c37fc5b53c6e5c2ea08f50b20cb992c6d
natalijasaveska <natalijasaveska@icloud.com>
2022-06-04 17:34:54 +0100

    homework 03 added

--- DB SQL 03-Aggregate Functions, Grouping and Views/03-homework.sql
+++ DB SQL 03-Aggregate Functions, Grouping and Views/03-homework.sql
@@ -10,39 +10,40 @@ SELECT * FROM GradeDetails
 -- Homework --
 
 -- Ex.1 --
-SELECT Teacher.ID,FirstName, COUNT(Grade) AS TotalGrades FROM Grade
+SELECT Teacher.ID, FirstName, COUNT(Grade) AS TotalGrades FROM Grade
 JOIN Teacher ON TeacherID = Teacher.ID
 GROUP BY  Teacher.ID,Teacher.FirstName
-ORDER BY Teacher.ID ASC
+ORDER BY TotalGrades;
+
 
 
 -- Ex.2 --
-SELECT Teacher.ID,Teacher.FirstName, COUNT(Grade) AS TotalGrades FROM Grade
+SELECT Teacher.ID, Teacher.FirstName, COUNT(Grade) AS TotalGrades FROM Grade
 JOIN Teacher ON TeacherID = Teacher.ID
 WHERE StudentID < 100
 GROUP BY  Teacher.ID,Teacher.FirstName
-ORDER BY FirstName
+ORDER BY ID;
 
 
 -- Ex.3 --
-SELECT Student.ID,Student.FirstName, MAX(Grade) AS MaxGrade, AVG(Grade) AS AverageGrade FROM Grade
+SELECT Student.ID, Student.FirstName, MAX(Grade) AS MaxGrade, AVG(Grade) AS AverageGrade FROM Grade
 JOIN Student ON Student.ID = Grade.StudentID
-GROUP BY  Student.ID,Student.FirstName
+GROUP BY Student.ID, Student.LastName, Student.FirstName
 
 
 --Ex.4 --
-SELECT Teacher.ID,Teacher.FirstName, COUNT(Grade) AS TotalGrades FROM Grade
+SELECT Teacher.ID, Teacher.FirstName, COUNT(Grade) AS TotalGrades FROM Grade
 JOIN Teacher ON TeacherID = Teacher.ID
-GROUP BY  Teacher.ID,Teacher.FirstName
+GROUP BY  Teacher.ID, Teacher.FirstName
 HAVING  COUNT(Grade) > 200
 ORDER BY ID;
 
 
 -- Ex.5 --
-SELECT Student.ID,Student.FirstName,COUNT(Grade) AS TotalGrades, MAX(Grade) AS MaxGrade, AVG(Grade) AS AverageGrade FROM Grade
+SELECT Student.ID, Student.FirstName, COUNT(Grade) AS TotalGrades, MAX(Grade) AS MaxGrade, AVG(Grade) AS AverageGrade FROM Grade
 JOIN Student ON Student.ID = Grade.StudentID
-GROUP BY  Student.ID,Student.FirstName
-HAVING MAX(Grade) = AVG(Grade)
+GROUP BY  Student.ID, Student.FirstName
+HAVING MAX(Grade) = AVG(Grade);
 
 
 -- Ex.6 --
@@ -58,9 +59,9 @@ SELECT * FROM  [dbo].vv_StudentGrades;
 -- Ex.7 --
 CREATE OR ALTER VIEW vv_StudentGrades
 AS
-SELECT Student.FirstName,Student.LastName, COUNT(Grade) AS TotalGrades FROM [dbo].Grade
+SELECT Student.FirstName, Student.LastName, COUNT(Grade) AS TotalGrades FROM [dbo].Grade
 JOIN [dbo].Student ON Grade.StudentID = Student.ID
-GROUP BY Student.FirstName,Student.LastName
+GROUP BY Student.FirstName, Student.LastName
 
 SELECT * FROM  vv_StudentGrades  
 ORDER BY TotalGrades DESC;
